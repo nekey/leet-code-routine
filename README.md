959427a0907d3e6f2cd04c62596c7cc4030f86d2
Nikolay Nezhevenko <n.nezhevenko@semrush.com>
2023-09-11 08:20:06 +0200

    Done 68 Text Justification

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # leet-code-routive
 Solve popular leet-code problems on my own using python, java & golang
 
+## TODO-list:
+* add pre-commit
+* add github actions - run tests
+* add description about tests
+* add how-to
--- src/150_tasks/68_text_justification.py
+++ src/150_tasks/68_text_justification.py
@@ -4,9 +4,10 @@ Link to task: https://leetcode.com/problems/text-justification/?envType=study-pl
 from typing import List
 
 
-class Solution:
+class SolutionOld:
+    """My first solution. It works, but slowly then others"""
     @staticmethod
-    def _make_line(words_chunk, max_width):
+    def _make_common_line(words_chunk, max_width):
         content_len = sum(len(word) for word in words_chunk)
         if len(words_chunk) == 1:
             return words_chunk[0].ljust(max_width)
@@ -14,24 +15,52 @@ class Solution:
         result = ""
         for idx in range(len(words_chunk) - 1):
             result += words_chunk[idx]
-            result += " " * (common_space_amount + 1 if idx <= large_space_span_count else common_space_amount)
+            result += " " * (common_space_amount + 1 if idx < large_space_span_count else common_space_amount)
         result += words_chunk[-1]
         return result
 
-    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        result = []
+    @staticmethod
+    def _make_last_line(words_chunk, max_width):
+        return " ".join(words_chunk).ljust(max_width)
+
+    def split_words_to_chunks(self, words: list[str], maxWidth: int) -> tuple[list[str], bool]:
+        """Yield chunk of words with lenght < 16 and flag "is_last" - is it last line or not"""
         words_chunk = []
         words_chunk_min_len = 0
-        for word in words:
-            if len(word) + words_chunk_min_len + 1 > maxWidth:
-                result.append(self._make_line(words_chunk, maxWidth))
+        for idx, word in enumerate(words):
+            actual_width = (len(word) + words_chunk_min_len + 1 if words_chunk_min_len else len(word))
+            if actual_width > maxWidth:
+                yield words_chunk, False
                 words_chunk = []
                 words_chunk_min_len = 0
             words_chunk.append(word)
             words_chunk_min_len += (len(word) if words_chunk_min_len == 0 else len(word) + 1)
         if words_chunk:
-            result.append(self._make_line(words_chunk, maxWidth))
+            yield words_chunk, True
+
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        result = []
+        for words_chunk, is_last in self.split_words_to_chunks(words, maxWidth):
+            if is_last:
+                result.append(self._make_last_line(words_chunk=words_chunk, max_width=maxWidth))
+            else:
+                result.append(self._make_common_line(words_chunk=words_chunk, max_width=maxWidth))
         return result
+
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        result, chunk, content_letters_count = [], [], 0
+        for word in words:
+            if content_letters_count + len(word) + len(chunk) > maxWidth:
+                for i in range(maxWidth - content_letters_count):
+                    word_in_chunk_idx = i % (len(chunk)-1 or 1)
+                    chunk[word_in_chunk_idx] += ' '
+                result.append(''.join(chunk))
+                chunk, content_letters_count = [], 0
+            chunk += [word]
+            content_letters_count += len(word)
+        return result + [' '.join(chunk).ljust(maxWidth)]
 
 
 def check_result(words, maxWidth, expected_justify):
@@ -67,5 +96,17 @@ def test_full_justify():
           "a  computer.  Art is",
           "everything  else  we",
           "do                  "
+        ],
+    )
+    check_result(
+        words=["Listen","to","many,","speak","to","a","few."],
+        maxWidth=6,
+        expected_justify=[
+            'Listen',
+            'to    ',
+            'many, ',
+            'speak ',
+            'to   a',
+            'few.  ',
         ],
     )
